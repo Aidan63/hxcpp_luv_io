64a3d7105924c9a68c9297621a3f1f4506748be2
Aidan Lee <aidan.lee63@gmail.com>
2022-06-18 16:28:30 +0100

    update readme

---
+++ README.md
@@ -0,0 +1,103 @@
+Libuv powered asynchronous IO and haxe event loop, more functions and classes will be needed as and when I need them.
+
+### File
+
+```haxe
+File.open(
+    'file.txt',
+    OpenMode.ReadWrite | OpenMode.Append,
+    AccessMode.UserReadWriteExecute,
+    result -> {
+        switch result {
+            case Success(file):
+                // 'file' is the object to manipulate the opened file.
+                // Below are some examples of file manipulation
+
+                file.read(result -> {
+                    switch result {
+                        case Success(bytes):
+                            // 'bytes' is a `haxe.io.Bytes` object containing the complete file contents.
+                        case Error(code):
+                            trace('failed to read file data : $code');
+                    }
+                });
+
+                file.write(Bytes.ofString('hello, world!'), result -> {
+                    switch result {
+                        case Success(count):
+                            // 'count' contains the number of bytes written to the file.
+                        case Error(code):
+                            trace('failed to write data : $code');
+                    }
+                });
+
+                file.close(result -> {
+                    switch result {
+                        case Some(v):
+                            trace('failed to close file $v');
+                        case None:
+                            trace('file closed');
+                    }
+                });
+            case Error(code):
+                trace('failed to open file : $code');
+        }
+    });
+```
+
+### Directory
+
+```haxe
+cpp.asio.Directory.open(
+    'C:/Some/Folder',
+    result -> {
+        switch result {
+            case Success(dir):
+                dir.iter(result -> {
+                    switch result {
+                        case Success(entry):
+                            trace(entry.name, entry.type);
+                        case Error(error):
+                            trace(error);
+
+                            dir.close(result -> {
+                                switch result {
+                                    case Some(error):
+                                        trace(error);
+                                    case None:
+                                        trace('dir closed');
+                                }
+                            });
+                    }
+                });
+            case Error(error):
+                trace(error);
+        }
+    });
+```
+
+### Process
+
+```haxe
+cpp.asio.Process.spawn(
+    'C:/HaxeToolkit/haxe.exe',
+    {
+        env: [ "MY_TEST_ENV" => "TRUE", "SOME_DEFINE" => null ],
+        args: [ '--help' ]
+    },
+    result -> {
+        switch result {
+            case Success(process):
+                process.stdout.read(result -> {
+                    switch result {
+                        case Success(bytes):
+                            Sys.print(bytes.toString());
+                        case Error(error):
+                            trace(error);
+                    }
+                });
+            case Error(error):
+                trace(error);
+        }
+    });
+```
--- src/Main.hx
+++ src/Main.hx
@@ -1,33 +1,38 @@
-import cpp.asio.Code;
+import sys.thread.EventLoop.EventHandler;
+import cpp.vm.Gc;
+import sys.thread.Thread;
 import cpp.asio.File;
 import cpp.asio.OpenMode;
 import cpp.asio.AccessMode;
 
 class Main {
     static function main() {
-        // Thread.current().events.repeat(() -> {
-        //     trace('gc time');
+        var handle : EventHandler = null;
 
-        //     Gc.run(true);
-        // }, 1000);
+        handle = Thread.current().events.repeat(() -> {
+            trace('gc time');
+
+            Gc.run(true);
+
+            Thread.current().events.cancel(handle);
+        }, 1000);
 
         cpp.asio.Process
             .spawn(
                 'C:/Users/AidanLee/Desktop/platformer/.flurry/haxe/Windows/haxe.exe',
                 {
-                    cwd: '',
-                    env: [ "MY_TEST_ENV" => "TRUE" ],
-                    args: []
+                    env: [ "MY_TEST_ENV" => "TRUE", "SOME_DEFINE" => null ],
+                    args: [ '--help' ]
                 },
                 result -> {
                     switch result {
-                        case Success(data):
-                            data.stdout.read(result -> {
+                        case Success(process):
+                            process.stdout.read(result -> {
                                 switch result {
-                                    case Success(data):
-                                        Sys.print(data.toString());
+                                    case Success(bytes):
+                                        Sys.print(bytes.toString());
                                     case Error(error):
-                                        trace(error);
+                                        //
                                 }
                             });
                         case Error(error):
@@ -35,57 +40,57 @@ class Main {
                     }
                 });
 
-        // cpp.asio.Directory.open('C:\\Users\\AidanLee\\Desktop\\hxcpp_luv\\src\\cpp\\asio', result -> {
-        //     switch result {
-        //         case Success(dir):
-        //             dir.iter(result -> {
-        //                 switch result {
-        //                     case Success(entry):
-        //                         trace(entry.name, entry.type);
-        //                     case Error(error):
-        //                         trace(error);
+        cpp.asio.Directory.open('C:\\Users\\AidanLee\\Desktop\\hxcpp_luv\\src\\cpp\\asio', result -> {
+            switch result {
+                case Success(dir):
+                    dir.iter(result -> {
+                        switch result {
+                            case Success(entry):
+                                trace(entry.name, entry.type);
+                            case Error(error):
+                                trace(error);
 
-        //                         dir.close(result -> {
-        //                             switch result {
-        //                                 case Some(v):
-        //                                     trace(v);
-        //                                 case None:
-        //                                     trace('dir closed');
-        //                             }
-        //                         });
-        //                 }
-        //             });
-        //         case Error(error):
-        //             trace(error);
-        //     }
-        // });
+                                dir.close(result -> {
+                                    switch result {
+                                        case Some(error):
+                                            trace(error);
+                                        case None:
+                                            trace('dir closed');
+                                    }
+                                });
+                        }
+                    });
+                case Error(error):
+                    trace(error);
+            }
+        });
 
-        // File.open('C:\\Users\\AidanLee\\Desktop\\hxcpp_luv\\test.txt', OpenMode.ReadWrite | OpenMode.Append, AccessMode.UserReadWriteExecute, result -> {
-        //     switch result {
-        //         case Success(file):
-        //             trace('file opened');
+        File.open('C:\\Users\\AidanLee\\Desktop\\hxcpp_luv\\test.txt', OpenMode.ReadWrite | OpenMode.Append, AccessMode.UserReadWriteExecute, result -> {
+            switch result {
+                case Success(file):
+                    trace('file opened');
 
-        //             file.read(result -> {
-        //                 switch result {
-        //                     case Success(data):
-        //                         trace(data.toString());
-        //                     case Error(error):
-        //                         trace(error);
-        //                 }
+                    file.read(result -> {
+                        switch result {
+                            case Success(data):
+                                trace(data.toString());
+                            case Error(error):
+                                trace(error);
+                        }
 
-        //                 file.close(result -> {
-        //                     switch result {
-        //                         case Some(v):
-        //                             trace('failed to close file $v');
-        //                         case None:
-        //                             trace('file closed');
-        //                     }
-        //                 });
-        //             });
+                        file.close(result -> {
+                            switch result {
+                                case Some(v):
+                                    trace('failed to close file $v');
+                                case None:
+                                    trace('file closed');
+                            }
+                        });
+                    });
 
-        //         case Error(code):
-        //             trace('failed to open file $code');
-        //     }
-        // });
+                case Error(code):
+                    trace('failed to open file $code');
+            }
+        });
     }
 }
